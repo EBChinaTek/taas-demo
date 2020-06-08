f5efd288518de63851ad86e598a9efb76f7b0c26
lchrennew <lchrennew@126.com>
2020-06-08 11:48:59 +0800

    Create index.js

---
+++ koa-server/src/index.js
@@ -0,0 +1,12 @@
+import Koa from "koa"
+import demo from "./controllers/demo";
+import bodyParser from 'koa-bodyparser';
+
+const router = new Router();
+demo(router);
+
+const app = new Koa();
+app
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods());
